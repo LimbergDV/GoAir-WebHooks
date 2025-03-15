76319dbfa54f652c0d0e70ac60c668a816cac8ca
LimbergDV <limbergdelvalle@gmail.com>
2025-03-15 11:45:31 -0600

    add: adding process_pull_request in application

--- application/process_pull_request.go
+++ application/process_pull_request.go
@@ -1,1 +1,34 @@
-package application
+package aplication
+
+import (
+	"GoAir-WebHooks/domain"
+	"encoding/json"
+	"fmt"
+	"log"
+)
+
+func ProcessPullRequestEvent(rawData []byte) (int, string) {
+	var eventPayload domain.PullRequestEventPayload
+
+	if err := json.Unmarshal(rawData, &eventPayload); err != nil {
+		return 403, " "
+	}
+
+	message := createMessage(eventPayload.PullRequest.Base.Ref, eventPayload.PullRequest.Head.Ref, eventPayload.PullRequest.User.Login, eventPayload.Repository.FullName)
+
+	if (eventPayload.Action == "closed") {
+		log.Printf("Se hace el pull hacia la rama: %s \n", eventPayload.PullRequest.Base.Ref)
+		log.Printf("El pull viene de la rama:  %s \n", eventPayload.PullRequest.Head.Ref)
+		log.Printf("Usuario: %s \n", eventPayload.PullRequest.User.Login)
+		log.Printf("Nombre del repositorio: %s \n", eventPayload.Repository.FullName)
+	}
+
+	return 200, message
+}
+
+func createMessage(base string, head string, user string, repository string) string {
+	return fmt.Sprint("Se hace el pull hacia la rama: "+ base + "\n",
+					   "El pull viene de la rama: "+ head + "\n", 
+					   "Usuario: "+ user + "\n",
+					   "Nombre del repositorio: "+ repository + "\n")
+}
